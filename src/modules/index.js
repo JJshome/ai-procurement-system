abc33d118fad479252c34f86667b28d5bc0253a4
JJshome <107289883+JJshome@users.noreply.github.com>
2025-05-06 23:25:44 +0900

    Create core modules index file

---
+++ src/modules/index.js
@@ -0,0 +1,18 @@
+/**
+ * AI-Powered Procurement System (AIPS)
+ * Core Modules Index
+ */
+
+const DataCollectionModule = require('./DataCollectionModule');
+const AIAnalysisModule = require('./AIAnalysisModule');
+const DocumentGenerationModule = require('./DocumentGenerationModule');
+const BlockchainModule = require('./BlockchainModule');
+const CollaborationModule = require('./CollaborationModule');
+
+module.exports = {
+  DataCollectionModule,
+  AIAnalysisModule,
+  DocumentGenerationModule,
+  BlockchainModule,
+  CollaborationModule
+};
