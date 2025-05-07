18ad6326559ff8ac0fb4f6a394e3441f3313dd8d
JJshome <107289883+JJshome@users.noreply.github.com>
2025-05-07 19:11:39 +0900

    Add utils index file

---
+++ src/utils/index.js
@@ -0,0 +1,15 @@
+/**
+ * AI-Powered Procurement System (AIPS)
+ * Utilities Index
+ * 
+ * This file exports all utility functions used throughout the system.
+ * 
+ * Based on Ucaretron Inc.'s patent technology for optimizing 
+ * public procurement bidding processes through artificial intelligence.
+ */
+
+const sampleDataLoader = require('./sampleDataLoader');
+
+module.exports = {
+  ...sampleDataLoader
+};
